b75e4b7183fd24a027db5aa91d4c34757172a34e
oliver-21 <115917220+oliver-21@users.noreply.github.com>
2024-05-26 16:49:04 +1000

    Update main.go

--- run/main.go
+++ run/main.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func sep(s string, r rune) (string, string) {
+	for i, e := range s {
+		if e == r {
+			return s[:i], s[i+1:]
+		}
+	}
+	return s, ""
+}
+
+func app(w http.ResponseWriter, r *http.Request) {
+	_, query := sep(r.URL.Path, '?')
+	fmt.Fprintf(w, "<DOCTYPE html><html>"+query+"</html>")
+
+}
+
+func main() {
+	http.HandleFunc("/", app)
+
+	// fmt.Printf("Starting server at port 5080")
+	if err := http.ListenAndServe(":80", nil); err != nil { //TODO: deal with $PATH variable as port number
+		panic(err)
+	}
+}
