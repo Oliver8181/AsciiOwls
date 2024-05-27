d3ef579d7e764b34d44a4ffe0be447c28c6f5140
oliver-21 <115917220+oliver-21@users.noreply.github.com>
2024-05-27 11:46:11 +1000

    Update main.go
    add in name cookie generation

--- run/main.go
+++ run/main.go
@@ -3,22 +3,55 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
-func sep(s string, r rune) (string, string) {
-	for i, e := range s {
-		if e == r {
-			return s[:i], s[i+1:]
-		}
-	}
-	return s, ""
+// func sep(s string, r rune) (string, string) {
+// 	for i, e := range s {
+// 		if e == r {
+// 			return s[:i], s[i+1:]
+// 		}
+// 	}
+// 	return s, ""
+// }
+
+func generate() string {
+	names := []string{"oliver", "mathew", "bob"}
+	rand := rand.New(rand.NewSource(time.Now().UnixNano() + 32383))
+	return names[rand.Int31n(int32(len(names)))]
+}
+
+func wrap(head, body string) string {
+	return "<!DOCTYPE html><html><head>" + head + "</head><body>" + body + "</body></html>"
 }
 
 func app(w http.ResponseWriter, r *http.Request) {
-	_, query := sep(r.URL.Path, '?')
-	fmt.Fprintf(w, "<DOCTYPE html><html>"+query+"</html>")
+	// _, query := sep(r.URL.Path, '?')
+	name, err := r.Cookie("name")
+	if err != nil {
+		name := generate()
+		http.SetCookie(w, &http.Cookie{
+			Name:  "name",
+			Value: name,
+
+			// Expires:  time.Time,
+
+			// MaxAge=0 means no 'Max-Age' attribute specified.
+			// MaxAge<0 means delete cookie now, equivalently 'Max-Age: 0'
+			// MaxAge>0 means Max-Age attribute present and given in seconds
+			MaxAge:   24 * 60 * 60,
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: http.SameSiteStrictMode,
+		})
+
+		fmt.Fprintf(w, wrap("", "We're going to call you "+name))
+	} else {
+		fmt.Fprintf(w, wrap("", "Welcome back "+name.Value))
+	}
 
 }
 
@@ -36,8 +69,7 @@ func main() {
 	http.HandleFunc("/", app)
 
 	// Determine port for HTTP service.
-	port := getPort()
-
+	port := getPort() // test on http://127.0.0.1:8080/
 	// Start HTTP server.
 	log.Printf("listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
