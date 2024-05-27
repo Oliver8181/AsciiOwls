e86452e63a8526bd0a6caa254c3a9b38113a6f06
oliver-21 <115917220+oliver-21@users.noreply.github.com>
2024-05-27 14:31:16 +1000

    Update main.go

--- run/main.go
+++ run/main.go
@@ -3,63 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
-	"time"
 )
 
-func sep(s string, r rune) (string, string) {
-	for i, e := range s {
-		if e == r {
-			return s[:i], s[i+1:]
-		}
+var replace = map[string]string{
+	"":     ".html",
+	".htm": ".html",
+}
+
+func pathFilter(path string) string {
+	extension := filepath.Ext(path)
+	if replacement, ok := replace[extension]; ok {
+		path = path[:len(path)-len(extension)] + replacement
 	}
-	return s, ""
-}
-
-func generate() string {
-	names := []string{"oliver", "mathew", "bob"}
-	rand := rand.New(rand.NewSource(time.Now().UnixNano() + 32383))
-	return names[rand.Int31n(int32(len(names)))]
-}
-
-func wrap(head, body string) string {
-	files, _ := filepath.Glob("*")
-	return "<!DOCTYPE html><html><head>" + head + "</head><body>" + body + "<br>" + strings.Join(files, ", ") + "</body></html>"
+	return path
 }
 
 func app(w http.ResponseWriter, r *http.Request) {
-	path, _ := sep(r.URL.Path, '?')
+	// if !filepath.IsAbs(r.URL.Path) {
+	// 	fmt.Println("error")
+	// 	return
+	// }
+	path := pathFilter(r.URL.Path)
 	data, err := os.ReadFile("site/" + path)
 	if err != nil {
-		name, err := r.Cookie("__session")
-		if err != nil {
-			name := generate()
-			http.SetCookie(w, &http.Cookie{
-				Name:  "__session",
-				Value: name,
-
-				// Expires:  time.Time,
-
-				// MaxAge=0 means no 'Max-Age' attribute specified.
-				// MaxAge<0 means delete cookie now, equivalently 'Max-Age: 0'
-				// MaxAge>0 means Max-Age attribute present and given in seconds
-				MaxAge:   24 * 60 * 60,
-				Secure:   true,
-				HttpOnly: true,
-				SameSite: http.SameSiteStrictMode,
-			})
-
-			fmt.Fprintf(w, wrap("", "We're going to call you "+name))
-		} else {
-			fmt.Fprintf(w, wrap("", "Welcome back "+name.Value))
-		}
-	} else {
-		fmt.Fprintf(w, "%s", data)
+		data, _ = os.ReadFile("site/404.html")
 	}
+	fmt.Fprintf(w, "%s", data)
 
 }
 
